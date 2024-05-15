a93be04350b07c647dce96e43d897e441c79d194
gmn26 <ghazynovrial@gmail.com>
2024-05-15 23:12:28 +0700

    fix query proses barang masuk

--- be/prosesbarangmasuk.php
+++ be/prosesbarangmasuk.php
@@ -4,10 +4,16 @@
         $barang = $_POST["nama_barang"];
         $jumlah_barang_masuk = $_POST["jumlah_barang"];
         $tgl_barang_masuk = $_POST["tgl_barang_masuk"];
-        $query = "INSERT INTO tb_barang_masuk(nama_barang, jumlah_barang_masuk, tgl_barang_masuk) VALUES ('$barang', $jumlah_barang_masuk, '$tgl_barang_masuk')";
+        $totalAwal = getStokSatuBarang($mysqli, $barang);
+        $totalAwal = $totalAwal["total_barang"];
+        $totalBarang = $jumlah_barang_masuk + $totalAwal;
+        
+        $queryTransaksi = "INSERT INTO tb_barang_masuk(nama_barang, banyak_barang_masuk, tgl_barang_masuk) VALUES ('$barang', '$jumlah_barang_masuk', '$tgl_barang_masuk')";
+        $queryUpdateStok = "UPDATE tb_stok_barang SET total = $totalBarang WHERE nama_barang = '$barang'";
 
         try{
-            mysqli_query($mysqli, $query);
+            mysqli_query($mysqli, $queryTransaksi);
+            mysqli_query($mysqli, $queryUpdateStok);
             
             if(mysqli_affected_rows($mysqli) > 0){
                 echo"
@@ -18,6 +24,7 @@
                 ";
             }
         }catch(Exception $e){
+            // echo $e;
             echo"
                 <script>
                     alert('Gagal menambahkan transaksi barang masuk')
